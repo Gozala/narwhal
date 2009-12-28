87f995e8e5fd62cd481c7693cea836956e8457b3
Irakli Gozalishvili <rfobic@gmail.com>
2009-12-28 23:17:37 +0100

    Some attempts to observe socket state

--- engines/rhino/lib/socket.js
+++ engines/rhino/lib/socket.js
@@ -1,11 +1,12 @@
 var IO = require("IO").IO;
 
+var JSelectorProvider = Packages.java.nio.channels.spi.SelectorProvider.provider();
 var JAddress = Packages.java.net.InetSocketAddress;
-var JSocketChannel = Packages.java.nio.channels.SocketChannel;
-var Buffer = java.nio.ByteBuffer.wrap;
+var JBuffer = java.nio.ByteBuffer.wrap;
 
 var J_OP_ACCEPT = Packages.java.nio.channels.SelectionKey.OP_ACCEPT;
 var J_OP_WRITE = Packages.java.nio.channels.SelectionKey.OP_WRITE;
+var J_OP_READ = Packages.java.nio.channels.SelectionKey.OP_READ;
 var J_OP_CONNECT = Packages.java.nio.channels.SelectionKey.OP_CONNECT;
 
 
@@ -19,7 +20,6 @@ Socket.prototype = new IO();
 Socket.prototype.readInto = function(buffer, length, from) {
     var bytes = buffer._bytes; // java byte array
     var offset = buffer._offset;
-    var buffer = Buffer(bytes);
     if (typeof from === "number")
         offset += from;
     if (length > bytes.length + offset)
@@ -27,32 +27,20 @@ Socket.prototype.readInto = function(buffer, length, from) {
     var total = 0,
         bytesRead = 0;
     while (total < length) {
-        bytesRead = this.__channel__.read(Buffer(bytes, offset + total, length - total));
+        bytesRead = this.__channel__.read(JBuffer(bytes, offset + total, length - total));
         if (bytesRead <= 0)
             break;
         total += bytesRead;
     }
     return total;
 };
-IO.prototype.writeInto = function(buffer, from, to) {
-    this.__channel__.write(Buffer(buffer._bytes, buffer._offset + from, to - from));
+Socket.prototype.writeInto = function(buffer, from, to) {
+    this.__channel__.write(JBuffer(buffer._bytes, buffer._offset + from, to - from));
     return this;
 };
-IO.prototype.copy = function (output, mode, options) {
-    while (true) {
-        var buffer = this.read(null);
-        if (!buffer.length)
-            break;
-        output.write(buffer);
-    }
-    output.flush();
+Socket.prototype.flush = function() {
     return this;
 };
-
-IO.prototype.flush = function() {
-    return this;
-};
-
 Socket.prototype.readyState = CONNECTING;
 Socket.prototype.constructor = Socket;
 /*
@@ -67,12 +55,14 @@ Socket.prototype.open = function open(host, port) {
     ['Opens the socket for communication\
     @param host:String      The host to connect to\
     @param port:Number      The port on which to connect']
-    var channel = this.__channel__ = JSocketChannel.open();
+    var channel = this.__channel__ = JSelectorProvider.openSocketChannel();
     channel.configureBlocking(false);
+    var selector = this.__selector__ = JSelectorProvider.openSelector();
+    this.__observer__ = channel.register(selector, J_OP_WRITE|J_OP_READ|J_OP_CONNECT);
+    this.__sync__ = new StateSync(this);
     channel.connect(new JAddress(host, port));
     channel.finishConnect();
-    // TODO: Avoid this ugly hack
-    this.inputStream = this.outputStream = channel;
+    this.__sync__.sync();
     return this;
 };
 Socket.prototype.onopen = function onopen() {
@@ -82,6 +72,7 @@ Socket.prototype.onopen = function onopen() {
 Socket.prototype.onmessage = function onmessage() {
     ['triggered when new data is arrived from the peer\
     this method is supposed to be overrided by socket user']
+    print("onmessage");
 };
 Socket.prototype.close = function close() {
     ['Opens the socket for communication']
@@ -90,6 +81,41 @@ Socket.prototype.close = function close() {
 Socket.prototype.onclose = function onclose() {
     ['triggered when connection has been closed.\
     this method is supposed to be overrided by socket server users']
+    print("onclose");
 };
 
-
+function StateSync(socket) {
+    this.socket = socket;
+}
+StateSync.prototype = {
+    constructor: StateSync,
+    sync: function sync() {
+        var socket = this.socket;
+        var observer = socket.__observer__;
+        var selector = socket.__selector__;
+        //setTimeout(function() {
+            while(observer.selector().select()) {
+                var event, events = selector.selectedKeys().toArray().slice();
+                while (event = events.shift()) {
+                    if (event.isValid()) {
+                        print("socket:valid")
+                    }
+                    if (event.isReadable()) {
+                        print("socket:readable");
+                        socket.read(); // need to buffer
+                    }
+                    if (event.isWritable()) {
+                        print("socket:writable")
+                    }
+                    if (event.isAcceptable()) {
+                        print("socket:acceptable")
+                    }
+                    if (event.isConnectable()) {
+                        print("socket:connectable")
+                    }
+                }
+            }
+            // sync();
+        //}, 10)
+    }
+};
